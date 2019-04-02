798a8082701a76ddda754302c779fe682d02d2ec
Matthew D Tse <tsemd@bu.edu>
2019-04-02 19:44:46 -0400

    changed some box types

--- src/javafxnpcgen/FXMLDocumentController.java
+++ src/javafxnpcgen/FXMLDocumentController.java
@@ -10,6 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 /**
@@ -22,9 +25,34 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button GenerateNPCButton;
+    
+    @FXML
+    private Button ResetButton;
+    
+    @FXML
+    private ComboBox<Integer> NumberOfNPCsSelect;
+    @FXML
+    private ChoiceBox<Integer> LevelSelect;
+    @FXML
+    private ChoiceBox<String> ClassSelect;
+    @FXML
+    private ChoiceBox<Integer> AbilityScoreLowSelect;
+    @FXML
+    private ChoiceBox<Integer> AbilityScoreHighSelect;
+    
+    @FXML
+    private void resetButtonAction(ActionEvent event) {
+        NumberOfNPCsSelect.setValue(1);
+        LevelSelect.setValue(1);
+        ClassSelect.setValue("");
+        AbilityScoreLowSelect.setValue(1);//1, 2–3, 4–5, 6–7, 8–9, 10–11, 12–13, 14–15, 16–17, 18–19, 20–21, 22–23, 24–25, 26–27, 28–29, 30
+        AbilityScoreHighSelect.setValue(-5);//−5, −4, −3, −2, −1, +0, +1, +2, +3, +4, +5, +6, +7, +8, +9, +10
+    }
+    
+    @FXML
+    private void exit(ActionEvent event) {
+        System.exit(0);
     }
     
     @Override
