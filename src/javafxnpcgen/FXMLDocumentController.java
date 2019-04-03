db5f92abf34865fc246f0aefe9dfc7481c65f724
Matthew D Tse <tsemd@bu.edu>
2019-04-02 20:00:34 -0400

    hardcoded local list

--- src/javafxnpcgen/FXMLDocumentController.java
+++ src/javafxnpcgen/FXMLDocumentController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import model.LocalItemList;
 
 /**
  *
